0eb4534f19d18f4ca04132df9dea0c5c2b28458e
Robert Domico <robert.domico@live.com>
2024-03-13 20:43:12 -0400

    Add project files.

---
+++ Rewards/Program.cs
@@ -0,0 +1,18 @@
+﻿using CrypticWizard.RandomWordGenerator;
+using System.Diagnostics;
+
+Random rnd = new();
+
+for (int counter = 0; counter < 40; counter++)
+{
+    WordGenerator wg = new();
+
+    string a = wg.GetWord(WordGenerator.PartOfSpeech.noun);
+    string url = "https://www.bing.com/search?q=" + a + "&form=QBI";
+
+    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+    if (counter % 3 == 0)
+        Thread.Sleep(rnd.Next(960000, 960000));
+    else
+        Thread.Sleep(rnd.Next(7000, 10000));
+}
