0b7e051b4b33bb5523fafd62020d1d28e39f4295
Robert Domico <robert.domico@live.com>
2024-07-18 15:09:35 -0400

    update for 15 minutes

--- Rewards/Program.cs
+++ Rewards/Program.cs
@@ -11,8 +11,9 @@ for (int counter = 0; counter < 40; counter++)
     string url = "https://www.bing.com/search?q=" + a + "&form=QBI";
 
     Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+   
     if (counter % 3 == 0)
-        Thread.Sleep(rnd.Next(960000, 960000));
+        Thread.Sleep(rnd.Next(900000, 960000));
     else
-        Thread.Sleep(rnd.Next(7000, 10000));
+        Thread.Sleep(rnd.Next(9000, 15000));
 }
